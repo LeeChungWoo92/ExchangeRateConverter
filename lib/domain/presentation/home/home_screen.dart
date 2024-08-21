8c39e98b8a1b27eb6548af8f3cdbe49e1e097094
Sangin Han <hugedreamhd@gmail.com>
2024-08-21 17:55:16 +0900

    feat: home_screen에서 input field로 입력값을 받는다

--- lib/domain/presentation/home/home_screen.dart
+++ lib/domain/presentation/home/home_screen.dart
@@ -1,50 +1,55 @@
+import 'package:exchange_rate_converter/domain/model/model.dart';
+import 'package:exchange_rate_converter/domain/presentation/component/input_field.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+void main() {
+  runApp(const MaterialApp(
+    home: HomeScreen(),
+  ));
+}
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '입력하세요',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                ),
-              ),
-              const SizedBox(
-                height: 15,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: '입력하세요',
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.grey),
-                  ),
-                ),
-              ),
+  _HomeScreenState createState() => _HomeScreenState();
+}
 
-            ],
-          ),
-        ),
-      ),
+class _HomeScreenState extends State<HomeScreen> {
+  final firstTextEditingController = TextEditingController();
+  final secondTextEditingController = TextEditingController();
+  String? selectedCountry;
+  final List<String> countryList = ['USA', 'Canada', 'South Korea', 'Japan'];
+
+  @override
+  void dispose() {
+    firstTextEditingController.dispose();
+    secondTextEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final data = HomeScreenData(
+      firstTextEditingController: firstTextEditingController,
+      secondTextEditingController: secondTextEditingController,
+      selectedCountry: selectedCountry,
+      countryList: countryList,
+    );
+
+    return InputField(
+      data: data,
+      onCountryChanged: (String? newCountry) {
+        setState(() {
+          selectedCountry = newCountry;
+        });
+      },
+      onFirstFieldChanged: (String value) {
+        print('First field changed: $value');
+      },
+      onSecondFieldChanged: (String value) {
+        print('Second field changed: $value');
+      },
     );
   }
 }
